7811be83d4596b26db10d4ef32ff77be720e8492
SamaraaTorres <samaratorres050@hotmail.com>
2022-09-29 06:36:35 -0300

    operações aritimeticas

---
+++ fundamentos/secao-4-intoducao-a-javascript/dia-1-javascript-primeiros-passos/exercicio.js
@@ -0,0 +1,10 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. 
+
+const a = 12;
+const b = 5;
+
+console.log( a + b);
+console.log( a - b);
+console.log( a * b);
+console.log( a / b);
+console.log( a % b);

44f03681de038c3451415493dedc86ce08012562
SamaraaTorres <samaratorres050@hotmail.com>
2022-09-29 20:17:04 -0300

    média dos itens

--- fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicio.js
+++ fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicio.js
@@ -15,6 +15,8 @@ console.log(soma)
 
 //Calcule e imprima a média aritmética dos valores contidos no array;
 
+let media = soma/numbers.length
+console.log(media)
 
 //Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
 

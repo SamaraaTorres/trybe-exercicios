e8f212176854fa7d93f59faa6e354fd8d88ace42
SamaraaTorres <samaratorres050@hotmail.com>
2022-10-09 16:49:05 -0300

    funções de maior valor

--- fundamentos/secao-4-introducao-a-javascript/dia-4-objetos-e-funcoes/fixar.js
+++ fundamentos/secao-4-introducao-a-javascript/dia-4-objetos-e-funcoes/fixar.js
@@ -32,3 +32,38 @@ let car = {
   for (let key in car) {
     console.log(key, car[key]);
   }
+//Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados.
+function adicao(a, b) {
+    let soma = a + b; 
+}
+function subtracao(a, b) {
+    let subitrai = a - b;
+}
+function multiplcacao(a, b) {
+    let multiplica = a * b;
+}
+function divisao(a, b) {
+    let divide = a / b;
+}
+function modulo(a, b) {
+    let resto = a % b;
+}
+//Faça um programa que retorne o maior de dois números. Defina, no começo do programa, duas constantes com os valores que serão comparados.
+function maiorValor(valor1, valor2) {
+    if (valor1 > valor2) {
+        console.log(valor1);
+    } else {
+        console.log(valor2);
+    }
+}
+
+//Faça um programa que retorne o maior de três números. Defina, no começo do programa, três constantes com os valores que serão comparados.
+function maiorValorDeTres(valor1, valor2, valor3) {
+    if (valor1 > valor2 && valor1 > valor3) {
+        console.log(valor1);
+    } else if (valor2 > valor1 && valor2 > valor3) {
+        console.log(valor2);
+    } else {
+        console.log(valor3);
+    }
+}

3d41d0815dd204016ad8a1d59de76c313d7ab74d
SamaraaTorres <samaratorres050@hotmail.com>
2022-10-14 00:27:02 -0300

    site adimistrador de tempo

---
+++ fundamentos/secao-5-javascript-dom-eventos-e-webstorage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,17 @@
+const header = document.querySelector('header');
+header.style.backgroundColor = 'mediumseagreen';
+
+const urgentes = document.querySelector('.emergency-tasks')
+urgentes.style.backgroundColor = 'lightSalmon';
+
+const naoUrgentes = document.querySelector('.no-emergency-tasks');
+naoUrgentes.style.backgroundColor = 'gold';
+
+const h3 = document.querySelectorAll('h3');
+h3[0].style.backgroundColor = 'blueviolet';
+h3[1].style.backgroundColor = 'blueviolet';
+h3[2].style.backgroundColor = 'black';
+h3[3].style.backgroundColor = 'black';
+
+const footer = document.querySelector('footer');
+footer.style.backgroundColor = 'darkslategray'

8a3abf8120a886cd38b6d32e345b2a8e3b99b658
SamaraaTorres <samaratorres050@hotmail.com>
2022-11-12 06:52:42 -0300

    sum teste

--- fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/scripts/sum.js
+++ fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/scripts/sum.js
@@ -1,1 +1,11 @@
 //Copie a função já implementada e desenvolva os testes. Separe a função e o teste em arquivos diferentes para evitar qualquer tipo de problema.
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+module.exports = sum;
--- fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/testes/sum.test.js
+++ fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/testes/sum.test.js
@@ -1,1 +1,8 @@
-//Copie a função já implementada e desenvolva os testes. Separe a função e o teste em arquivos diferentes para evitar qualquer tipo de problema.
+const sum = require('../scripts/sum');
+
+it('A função sum(a, b) retorna a soma do parâmetro a com o b', () => {
+    expect(sum(4, 5)).toBe(9);
+    expect(sum(0, 0)).toBe(0);
+    expect(() => sum('4', 5)).toThrow();
+    expect(() => sum(4, '5')).toThrow(/^parameters must be numbers$/);
+})
